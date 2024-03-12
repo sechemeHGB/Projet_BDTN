e8fb1666233a5bededbf9da75c4998aea73e3ad3
HerveTng <162592956+HerveTng@users.noreply.github.com>
2024-03-12 11:10:21 +0100

    ajout des classes

--- Projet_BDTN/Models/Console.cs
+++ Projet_BDTN/Models/Console.cs
@@ -2,6 +2,9 @@
 {
     public class Console
     {
+        public int Id { get; set; }
+        public ICollection<Vente> Ventes { get; set; }
         public string Name { get; set; }
+        public string Constructors {  get; set; }
     }
 }
---
+++ Projet_BDTN/Models/Vente.cs
@@ -0,0 +1,9 @@
+﻿namespace Projet_BDTN.Models
+{
+    public class Vente
+    {
+        public int id { get; set; }
+        public int year { get; set; }
+        public decimal amount { get; set; }
+    }
+}
